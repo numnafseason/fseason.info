b9c51650ac942b51ebfa0515c2a6665368696ca1
hyuckry <hyuckry@gmail.com>
2022-08-04 14:11:51 +0900

    lint config add

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # fseason.info
+
+## JEST 테스트 프래임워크 사용
+
+### 테스트 모니터링 모드 
+```powershell
+npx jest --watch
+```
+### 테스트 커버리지 확인
+```powershell
+npx jest --coverage   
+```
+
+- puppeteer - 헤드리스 크롬(화면에 UI를 그리지 않고 browser 실행) 설치
+- portfinder - 사용가능한 포트를 찾아주는 유틸
+```powershell
+npm i --save-dev puppeteer
+npm i --save-dev portfinder
+```
+
+## 린트
+```powershell
+npm i --save-dev eslint
+npm i --save-dev eslint-plugin-jest
+```
+- "lint":"eslint meadowlark.js lib" pakage.json 추가
+
+#경로
+image.png
--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars');
-const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -15,23 +15,18 @@ app.use(express.static(__dirname + '/public'))
 const port = process.env.PORT || 3000
 
  
-app.get('/', (req, res) => res.render('home'));
-app.get('/about', (req, res) => {
-    res.render('about',{fortune:fortune.getFortune()}) 
-});
+app.get('/', handlers.home);
+app.get('/about', handlers.about);
 // custom 404 page
-app.use((req, res) => { 
-    res.status(404)
-    res.render('404')
-})
-
+app.use(handlers.notFound);
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message) 
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError);
 
+// node에서 직접 실행한 경우가 아니면 eport 
+if(require.main === module){
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
     `press Ctrl-C to terminate.`))
+}else{
+    module.exports = app;
+}
