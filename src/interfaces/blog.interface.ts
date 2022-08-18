7604f6a8c4b466676b4a174d08362a41f327acc7
PaulMac <nhyuckry@naver.com>
2022-08-18 15:54:41 +0900

    blog add

---
+++ src/interfaces/blog.interface.ts
@@ -0,0 +1,8 @@
+export interface Blog {
+  id: number;
+  title: string;
+  summary: string;
+  body: string;
+  date: string;
+  author_id: number;
+}
---
+++ src/models/posts.model.ts
@@ -0,0 +1,16 @@
+import { Model, ModelObject } from 'objection';
+import { Blog } from '@/interfaces/blog.interface';
+
+export class Posts extends Model implements Blog {
+  id!: number;
+  title!: string;
+  summary!: string;
+  body!:string;
+  date!:string;
+  author_id!:number;
+
+  static tableName = 'posts'; // database table name
+  static idColumn = 'id'; // id column name
+}
+
+export type PostsShape = ModelObject<Posts>;
